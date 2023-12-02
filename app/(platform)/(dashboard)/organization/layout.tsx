c3fa362ec77b56a70911ac2dfd0f77538a6f10f3
kunal-simplifii <kunal.arya@simplifii.com>
2023-12-02 08:48:11 +0530

    feat: Sidebar created

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "../_components/sidebar"
 
 
 const OrganizationLayout = ({ children }: {
@@ -7,10 +8,10 @@ const OrganizationLayout = ({ children }: {
         <div className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
             <div className="flex gap-x-7">
                 <div className="w-64 shrink-0 hidden md:block">
-                    {/* Sidebar */}
+                    <Sidebar />
                 </div>
+                {children}
             </div>
-            {children}
         </div>
     )
 }
