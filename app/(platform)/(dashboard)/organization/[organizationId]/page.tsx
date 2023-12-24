c7c7ae5efebc0b427bac96e55870e10d3d50553f
kunal-arya <kunalaryad@gmail.com>
2023-12-24 13:59:07 +0530

    feat: Hint Component Created

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,6 @@
 import { Separator } from "@/components/ui/separator"
 import { Info } from "./_components/info"
+import BoardList from "./_components/board-list"
 
 const OrganizationIdPage = async () => {
 
@@ -9,7 +10,7 @@ const OrganizationIdPage = async () => {
             <Info />
             <Separator className="my-4" />
             <div className="px-2 md:px-4">
-                {/* <BoardList /> */}
+                <BoardList />
             </div>
         </div>
     )
