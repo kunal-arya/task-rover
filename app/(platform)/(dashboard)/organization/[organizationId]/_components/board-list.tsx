e19b7580f949ad482f52c56675baec72833c229c
kunal-arya <kunalaryad@gmail.com>
2023-12-24 14:56:46 +0530

    feat: Form popover & Board list component added

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,5 +1,7 @@
-import { User2 } from "lucide-react"
+import { HelpCircle, User2 } from "lucide-react"
 
+import { Hint } from "@/components/hint"
+import { FormPopover } from "@/components/form/form-popover"
 
 const BoardList = () => {
     return (
@@ -9,15 +11,25 @@ const BoardList = () => {
                 Your Boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div
-                    role="button"
-                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-                >
-                    <p className="tex-sm">Create new Board</p>
-                    <span className="text-xs">
-                        5 remaining
-                    </span>
-                </div>
+                <FormPopover sideOffset={10} side={"right"}>
+                    <div
+                        role="button"
+                        className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                    >
+                        <p className="tex-sm">Create new Board</p>
+                        <span className="text-xs">
+                            5 remaining
+                        </span>
+                        <Hint
+                            sideOffset={40}
+                            description={
+                                `Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`
+                            }
+                        >
+                            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
         </div>
     )
